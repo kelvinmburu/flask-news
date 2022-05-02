074ae1ea8abcf184415aecc142d2ab065a525ebe
kelvinmburu <mburuhkelvin@gmail.com>
2022-05-02 15:51:12 +0300

    chore: added a new class Article

---
+++ app/models/articles.py
@@ -0,0 +1,14 @@
+class Article:
+    '''
+    Movie class to define Movie Objects
+    '''
+
+    def __init__(self,author,title,description,url,urlToImage,publishedAt,content):
+        self.author = author
+        self.title = title
+        self.description = description
+        self.url = url
+        self.urlToImage = urlToImage
+        self.publishedAt = publishedAt
+        self.content = content
+    pass
