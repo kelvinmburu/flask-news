31e8d0c15c4476cc3a788eb95eb0ad73110f028e
kelvinmburu <mburuhkelvin@gmail.com>
2022-05-02 15:52:06 +0300

    fix: fixed typo errors by replacing articles

--- app/request.py
+++ app/request.py
@@ -1,4 +1,58 @@
 from app import app
+import urllib.request,json
+from .models import articles
+
+Articles = articles.Articles
 
 # Getting api key
 api_key = app.config['NEWS_API_KEY']
+
+# Getting the movie base url
+base_url = app.config["NEWS_API_BASE_URL"]
+
+def get_article_top_headlines(category):
+    '''
+    Function that gets the json response to our url request
+    '''
+    get_articles_url = base_url.format(category,api_key)
+
+    with urllib.request.urlopen(get_articles_url) as url:
+        get_articles_data = url.read()
+        get_articles_response = json.loads(get_articles_data)
+
+        articles_results = None
+
+        if get_articles_response['articles']:
+            articles_results_list = get_articles_response['articles']
+            articles_results = process_results(articles_results_list)
+
+
+    return articles_results
+
+def process_results(articles_list):
+    '''
+    Function  that processes the articles result and transform them to a list of Objects
+
+    Args:
+        articles_list: A list of dictionaries that contain movie details
+
+    Returns :
+        articles_results: A list of movie objects
+    '''
+    articles_results = []
+    
+    for article in articles_list:
+        author = article.get('author')
+        title = article.get('title')
+        description = article.get('description')
+        url = article.get('url')
+        urlToImage = article.get('urlToImage')
+        publishedAt = article.get('publishedAt')
+        content = article.get('content')
+      
+
+        if urlToImage:
+            article_object = Articles(author,title,description,url,urlToImage,publishedAt,content)
+            articles_results.append(article_object)
+
+    return articles_results
