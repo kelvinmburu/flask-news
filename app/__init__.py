9801f7bcb5c3d5c1ae88251d98cd316719ab5b16
kelvinmburu <mburuhkelvin@gmail.com>
2022-05-03 02:03:19 +0300

    chore imported error for error view page

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from app.config import DevConfig
 from flask_bootstrap import Bootstrap
 
 # Initializing application
-app = Flask(__name__,instance_relative_config=True)
+app = Flask(__name__, instance_relative_config=True)
 
 # Setting up configuration
 app.config.from_object(DevConfig)
@@ -12,4 +12,5 @@ app.config.from_pyfile('config.py')
 # Initializing Flask Extensions
 bootstrap = Bootstrap(app)
 
+from app import error
 from app import views
