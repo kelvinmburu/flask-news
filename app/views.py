5f7ea7389b90b92ceee67a5fe892fe99db581224
kelvinmburu <mburuhkelvin@gmail.com>
2022-05-02 13:40:29 +0300

    chore: added a new route news

--- app/views.py
+++ app/views.py
@@ -8,4 +8,14 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    return render_template('index.html')
+    hello = 'Hello kelvin'
+    return render_template('index.html',hello = hello)
+
+@app.route('/news/<int:news_id>')
+def news(news_id):
+    
+    '''
+    View news section that returns the movie details and its data.
+    '''
+    
+    return render_template('news.html',id = news_id)
