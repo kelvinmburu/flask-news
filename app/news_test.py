6d564ac7566b01f209eccb7a7c865b4a13b26cb5
kelvinmburu <mburuhkelvin@gmail.com>
2022-05-02 15:52:23 +0300

    feat: changed class to Articles

--- app/news_test.py
+++ app/news_test.py
@@ -1,8 +1,8 @@
 import unittest
-from models import news
-News = news.News
+from models import article
+Articles = article.Articles
 
-class NewsTest(unittest.TestCase):
+class ArticlesTest(unittest.TestCase):
     '''
     Test Class to test the behaviour of the Movie class
     '''
@@ -11,10 +11,10 @@ class NewsTest(unittest.TestCase):
         '''
         Set up method that will run before every Test
         '''
-        self.new_news = News(1234,'Python Must Be Crazy','A thrilling new Python Series','https://image.tmdb.org/t/p/w500/khsjha27hbs',8.5,129993)
+        self.new_articles = Articles(1234,'Python Must Be Crazy','A thrilling new Python Series','https://image.tmdb.org/t/p/w500/khsjha27hbs',8.5,129993)
 
     def test_instance(self):
-        self.assertTrue(isinstance(self.new_news,News))
+        self.assertTrue(isinstance(self.new_articles,Articles))
 
 
 if __name__ == '__main__':
