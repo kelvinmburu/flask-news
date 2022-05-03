42d163633fe1e0dcac499c4b53e22b4d0cc2f821
kelvinmburu <mburuhkelvin@gmail.com>
2022-05-03 09:51:10 +0300

    feat: updated init file

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,21 @@
 from flask import Flask
-from config import DevConfig
 from flask_bootstrap import Bootstrap
+from config import config_options
 
-# Initializing application
-app = Flask(__name__, instance_relative_config=True)
+bootstrap = Bootstrap()
 
-# Setting up configuration
-app.config.from_object(DevConfig)
-app.config.from_pyfile('config.py')
+def create_app(config_name):
+    
+    app = Flask(__name__)
+    
+    #Creating the app configurations
+    app.config.from_object(config_options[config_name])
 
-# Initializing Flask Extensions
-bootstrap = Bootstrap(app)
 
-from app import error
-from app import views
+    # Initializing Flask Extensions
+    bootstrap = Bootstrap(app)
+
+    from app import error
+    from app import views
+
+    return app
