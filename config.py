9866b2ff62b7429a0b73fe77f6978a0fe75f9cf3
kelvinmburu <mburuhkelvin@gmail.com>
2022-05-04 09:22:00 +0300

    fx: idented code

--- config.py
+++ config.py
@@ -23,7 +23,7 @@ class DevConfig(Config):
         Config: The parent configuration class with General configuration settings
     '''
 
-    DEBUG = True
+DEBUG = True
     
 config_options = {
     'development':DevConfig,
